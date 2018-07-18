7a561292185c59f3515a3ecd6e1903eec7351b58
Hok S. Uy <hoksuy@gmail.com>
2018-07-18 12:21:05 -0700

    Modified assign and remove admin rights, created helper functions, modified UI.

--- ADapp/ADapp/MainWindow.xaml.cs
+++ ADapp/ADapp/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace ADapp
         private string computerName;
         private ADAddress domainList;
         private int domainID;
+        private bool permanent;
         public MainWindow(string u, string p, ADAddress a)
         {
             InitializeComponent();
@@ -50,7 +51,11 @@ namespace ADapp
                                                              userid, password, AuthenticationTypes.Secure))
                 {
                     OS.Text = entry.Properties["OperatingSystem"].Value.ToString() + " " + entry.Properties["OperatingSystemVersion"].Value.ToString();
-                    ManagedBy.Text = entry.Properties["ManagedBy"].Value.ToString();
+                    if (entry.Properties.Contains("ManagedBy"))
+                        ManagedBy.Text = entry.Properties["ManagedBy"].Value.ToString();
+                    else
+                        ManagedBy.Text = "";
+                    Description.Text = entry.Properties["Description"].Value.ToString();
                     entry.Close();
                 }
             } catch(Exception ex)
@@ -77,6 +82,7 @@ namespace ADapp
                 {
                     //do something with user
                     displayname.Text = entry.Properties["DisplayName"].Value.ToString();
+                    entry.Close();
                 }
             } catch(Exception ex)
             {
@@ -96,6 +102,7 @@ namespace ADapp
                     entry.Invoke("SetPassword", new object[] { "Welcome1" });
                     entry.Properties["LockOutTime"].Value = 0;
                     entry.CommitChanges();
+                    entry.Close();
 
                     System.Windows.MessageBox.Show("Password has been reset");
                 }
@@ -117,6 +124,7 @@ namespace ADapp
                     //do something with user
                     entry.Properties["LockOutTime"].Value = 0;
                     entry.CommitChanges();
+                    entry.Close();
 
                     System.Windows.MessageBox.Show("Account unlocked");
                 }
@@ -139,6 +147,7 @@ namespace ADapp
                     string descrip = Description.Text;
                     entry.InvokeSet("description", descrip);   //Uncomment and modify this line if you want to play around
                     entry.CommitChanges();
+                    entry.Close();
 
                     System.Windows.MessageBox.Show("Successfully changed description");
                 }
@@ -159,12 +168,22 @@ namespace ADapp
                                                              userid, password, AuthenticationTypes.Secure))
                 {
                     string useradm = useradm_assign.Text;
+                    string tempStr;
                     DirectoryEntry entry2 = new DirectoryEntry("LDAP://CN=" + useradm + domainList.domains[domainID].DNuser, userid, password, AuthenticationTypes.Secure);
 
                     entry.Properties["managedby"].Clear();
                     entry.Properties["managedby"].Add(entry2.Properties["distinguishedname"].Value);
                     //System.Windows.MessageBox.Show("An exception was thrown because:\n" + entry);
+                    tempStr = editRemove(entry.Properties["description"].Value.ToString());
+                    tempStr = editAdd(tempStr);
+                    entry.Properties["description"].Clear();
+                    entry.Properties["description"].Add(tempStr);
                     entry.CommitChanges();
+                    Description.Text = entry.Properties["description"].Value.ToString();
+                    if (entry.Properties.Contains("ManagedBy"))
+                        ManagedBy.Text = entry.Properties["ManagedBy"].Value.ToString();
+                    else
+                        ManagedBy.Text = "";
                     entry.Close();
 
                     System.Windows.MessageBox.Show("Added " + useradm + " as admin for " + computerName);
@@ -185,17 +204,51 @@ namespace ADapp
                 using (DirectoryEntry entry = new DirectoryEntry("LDAP://CN=" + computerName + domainList.domains[domainID].DNcomputer,
                                                              userid, password, AuthenticationTypes.Secure))
                 {
-
+                    string tempStr;
                     entry.Properties["managedby"].Clear();
+                    tempStr = editRemove(entry.Properties["description"].Value.ToString());
+                    entry.Properties["description"].Clear();
+                    entry.Properties["description"].Add(tempStr);
                     entry.CommitChanges();
-
+                    Description.Text = entry.Properties["description"].Value.ToString();
+                    if (entry.Properties.Contains("ManagedBy"))
+                        ManagedBy.Text = entry.Properties["ManagedBy"].Value.ToString();
+                    else
+                        ManagedBy.Text = "";
                     System.Windows.MessageBox.Show("Removed admin for " + computerName);
+                    entry.Close();
                 }
             }
             catch (Exception ex)
             {
                 System.Windows.MessageBox.Show("An exception was thrown because:\n" + ex.Message);
             }
+        }
+
+        private string editAdd(string s)
+        {
+            DateTime today = DateTime.Today;
+
+            if (permanent)
+                s = "*" + s;
+            else
+                s = "*****" + s + "Remove temp admin rights after " + today.ToString("d");
+            return s;
+        }
+
+        private string editRemove(string s)
+        {
+            s = s.Substring(s.LastIndexOf('*') + 1);
+            s = s.Substring(0, s.LastIndexOf(',') + 1);
+            return s;
+        }
+
+        private void perm_Checked(object sender, RoutedEventArgs e)
+        {
+            if (perm.IsChecked.Value == true)
+                permanent = true;
+            else
+                permanent = false;
         }
     }
 }
